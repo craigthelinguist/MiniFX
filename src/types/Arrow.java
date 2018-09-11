dc83e84772436ca5cbbf9ca5b4ba171b25468cbe
craigthelinguist <aaroncraig@protonmail.ch>
2018-09-11 20:36:03 +1200

    Delete broken code, get tests to run

--- src/types/Arrow.java
+++ src/types/Arrow.java
@@ -5,45 +5,35 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import com.sun.scenario.effect.Effect;
-
 public class Arrow implements Type {
 
 	private Type[] inputTypes;
 	private Type outputType;
-	private Set<Effect> effects;
 	
 	public static Arrow Pure(List<Type> inputTypes, Type outputType) {
-		return new Arrow(inputTypes, outputType, new HashSet<>());
+		return new Arrow(inputTypes, outputType);
 	}
 	
-	public Arrow(List<Type> inputTypes, Type outputType, Set<Effect> fx) {
+	public static Arrow Pure(Type[] inputTypes, Type outputType) {
+		return new Arrow(inputTypes, outputType);
+	}
+	
+	public static Arrow Pure(Type inputType, Type outputType) {
+		Type[] inputTypes = new Type[] { inputType };
+		return Arrow.Pure(inputTypes, outputType);
+	}
+	
+	public Arrow(List<Type> inputTypes, Type outputType) {
 		this.inputTypes = new Type[inputTypes.size()];
 		for (int i = 0; i < inputTypes.size(); i++) {
 			this.inputTypes[i] = inputTypes.get(i);
 		}
 		this.outputType = outputType;
-		this.effects = fx;
 	}
 	
-	public static Arrow Pure(Type inputType, Type outputType) {
-		return new Arrow(inputType, outputType, new HashSet<>());
-	}
-	
-	public Arrow(Type inputType, Type outputType, Set<Effect> fx) {
-		this.inputTypes = new Type[]{ inputType };
-		this.outputType = outputType;
-		this.effects = fx;
-	}
-
-	public static Arrow Pure(Type[] inputTypes, Type outputType) {
-		return new Arrow(inputTypes, outputType, new HashSet<>());
-	}
-	
-	public Arrow(Type[] inputTypes, Type outputType, Set<Effect> fx) {
+	public Arrow(Type[] inputTypes, Type outputType) {
 		this.inputTypes = inputTypes;
 		this.outputType = outputType;
-		this.effects = fx;
 	}
 
 	public Type[] inputTypes() {
@@ -61,8 +51,6 @@ public class Arrow implements Type {
 		if (arr.inputTypes.length != inputTypes.length)
 			return false;
 		if (!this.outputType().subtypeOf(arr.outputType))
-			return false;
-		if (!this.effects.containsAll(arr.effects))
 			return false;
 		for (int i = 0; i < inputTypes.length; i++) {
 			if (!arr.inputTypes[i].subtypeOf(this.inputTypes[i]))
@@ -97,6 +85,11 @@ public class Arrow implements Type {
 
 	public int numArgs() {
 		return inputTypes.length;
+	}
+
+	public static Type Pure(Int intType, Int intType2) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
